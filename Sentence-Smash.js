be1704a19b501f1803ad1d7a02ddb8a38773037e
Liverpool4ik <liverpool4ik@proton.me>
2024-02-21 23:07:24 +0200

    Sentence Smash

--- Sentence-Smash.js
+++ Sentence-Smash.js
@@ -1,14 +1,17 @@
-//// TASK NAME
+////Sentence Smash
+
 // // 🎹
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
 
 // ⚽ Solution
 
 // 1)================================== -->
 
-// function .... {
-
-// }
-
+function smash(words) {
+	return words.join(" ");
+}
 // 3)==================================
 
 // 4)==================================
