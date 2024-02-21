bae658e26ee70a70120c214a3adf33091897f3f9
Liverpool4ik <liverpool4ik@proton.me>
2024-02-21 23:53:49 +0200

    JS-Cave-Dragon - Step 42

---
+++ Count-by-X.js
@@ -0,0 +1,42 @@
+//// TCount by X
+
+// // // 🎹
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater
+// Return the results as an array or list ( depending on language ).
+
+// than 0.
+
+// Examples;
+// countBy(1, 10) === [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// countBy(2, 5) === [2, 4, 6, 8, 10];
+
+// // ⚽ Solution
+
+// 1)================================== -->
+
+function countBy(x, n) {
+	let result = [];
+	let times = n;
+	for (let i = x; times > 0; i += x) {
+		times--;
+	}
+	return result;
+}
+
+// 3)==================================
+
+// 4)==================================
+
+// 5)==================================
+
+// 6)==================================
+
+// 7)==================================
+
+//8)==================================
+
+//9)==================================
+
+//10)==================================
