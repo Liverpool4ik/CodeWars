3e35fb0f00bb0a50dba328fedecef170cb3b35d9
Liverpool4ik <liverpool4ik@proton.me>
2024-02-23 13:35:00 +0200

    Convert boolean values to strings

--- Sum-of-positive.js
+++ Sum-of-positive.js
@@ -19,13 +19,41 @@ function positiveSum(arr) {
 }
 
 // 2) ==================================   ---->
-
+function positiveSum(arr) {
+	var total = 0;
+	for (i = 0; i < arr.length; i++) {
+		// setup loop to go through array of given length
+		if (arr[i] > 0) {
+			// if arr[i] is greater than zero
+			total += arr[i]; // add arr[i] to total
+		}
+	}
+	return total; // return total
+}
 // 3)==================================  ---->
-
+function positiveSum(arr) {
+	return arr.filter((x) => x >= 0).reduce((a, c) => a + c, 0);
+}
 // 4)==================================  ---->
-
+function positiveSum(arr) {
+	let sum = 0;
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > 0) {
+			sum += arr[i];
+		}
+	}
+	return sum;
+}
 // 5)==================================  ---->
-
+function positiveSum(arr) {
+	let sum = 0;
+	arr.forEach(function (v, i, a) {
+		if (v > 0) {
+			sum += v;
+		}
+	});
+	return sum;
+}
 // 6)==================================  ---->
 
 // 7)==================================  ---->
