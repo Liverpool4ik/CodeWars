9d7aa31dbdeec0f785f56897c6ca6fc2e546eae8
Liverpool4ik <liverpool4ik@proton.me>
2024-02-24 23:32:20 +0200

    8.Kata - Square(n) Sum

---
+++ Square(n)-Sum.js
@@ -0,0 +1,34 @@
+//// Square(n) Sum
+// // // 🎹Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9 because
+
+function squareSum(numbers) {}
+// ⚽ Solution
+
+// 1)================================== -->
+
+let arrLength = numbers.length;
+let summ = 0;
+for (let i = 0; i < arrLength; i++) {
+	let squareNumbers = Number(numbers[i] ** 2);
+	summ = summ + squareNumbers;
+}
+return summ;
+// 2) ==================================   ---->
+
+// 3)==================================  ---->
+
+// 4)==================================  ---->
+
+// 5)==================================  ---->
+
+// 6)==================================  ---->
+
+// 7)==================================  ---->
+
+//8)==================================  ---->
+
+//9)==================================  ---->
+
+//10)==================================  ---->
