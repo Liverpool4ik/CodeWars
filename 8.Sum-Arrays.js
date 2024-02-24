f7304d12afe75d07420c73a00c259b49007d2c49
Liverpool4ik <liverpool4ik@proton.me>
2024-02-25 00:54:52 +0200

    ///8.Sum-Arrays

---
+++ 8.Sum-Arrays.js
@@ -0,0 +1,33 @@
+///8.Sum-Arrays
+// // 🎹Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+// ⚽ Solution
+// Sum Numbers
+function sum(numbers) {
+	"use strict";
+	if (numbers.length === 0) return 0;
+	return numbers.reduce((total, currentValue) => total + currentValue);
+}
+// 1)================================== -->
+
+// function .... {
+
+// }
+
+// 2) ==================================   ---->
+
+// 3)==================================  ---->
+
+// 4)==================================  ---->
+
+// 5)==================================  ---->
+
+// 6)==================================  ---->
+
+// 7)==================================  ---->
+
+//8)==================================  ---->
+
+//9)==================================  ---->
+
+//10)==================================  ---->
