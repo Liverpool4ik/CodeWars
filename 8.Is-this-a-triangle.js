e5a0ec8fb9e913c01d9188bcd86e3d83de995835
Liverpool4ik <liverpool4ik@proton.me>
2024-02-24 23:42:58 +0200

    //8.Is this a triangle?

---
+++ 8.Is-this-a-triangle.js
@@ -0,0 +1,32 @@
+//8.Is this a triangle?
+
+// // // 🎹DESCRIPTION:
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+function isTriangle(a, b, c) {
+	return false;
+}
+// ⚽ Solution
+function isTriangle(a, b, c) {
+	return a + b > c && a + c > b && b + c > a ? true : false;
+}
+// 1)================================== -->
+
+// 2) ==================================   ---->
+
+// 3)==================================  ---->
+
+// 4)==================================  ---->
+
+// 5)==================================  ---->
+
+// 6)==================================  ---->
+
+// 7)==================================  ---->
+
+//8)==================================  ---->
+
+//9)==================================  ---->
+
+//10)==================================  ---->
