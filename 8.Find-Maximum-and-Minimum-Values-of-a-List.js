88b7cacc779e1d4c3c2f797ca6539fd2c9c2a12d
Liverpool4ik <liverpool4ik@proton.me>
2024-02-25 00:38:04 +0200

    //8.Find-Maximum-and-Minimum-Values-of-a-List.js

---
+++ 8.Find-Maximum-and-Minimum-Values-of-a-List.js
@@ -0,0 +1,34 @@
+//8.Find-Maximum-and-Minimum-Values-of-a-List.js
+
+// // 🎹 DESCRIPTION:
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+// Examples (Input -> Output)
+
+// ⚽ Solution
+
+// 1)================================== -->
+
+const min = (list) => {
+	return Math.min(...list);
+};
+const max = (list) => {
+	return Math.max(...list);
+};
+// 2) ==================================   ---->
+
+// 3)==================================  ---->
+
+// 4)==================================  ---->
+
+// 5)==================================  ---->
+
+// 6)==================================  ---->
+
+// 7)==================================  ---->
+
+//8)==================================  ---->
+
+//9)==================================  ---->
+
+//10)==================================  ---->
