47a6aaa964dd9d5bc7a5433c1a9ba80d7fedc1ed
Liverpool4ik <liverpool4ik@proton.me>
2024-02-25 20:22:38 +0200

    //8.If-you-can't-sleep,-just-count-sheep

--- 6.Build-Tower.js
+++ 6.Build-Tower.js
@@ -24,19 +24,63 @@ function towerBuilder(nFloors) {
 	return result;
 }
 // 1)================================== -->
-
-// function .... {
-
-// }
-
+function towerBuilder(nFloors) {
+	var tower = [];
+	for (var i = 0; i < nFloors; i++) {
+		tower.push(
+			" ".repeat(nFloors - i - 1) +
+				"*".repeat(i * 2 + 1) +
+				" ".repeat(nFloors - i - 1)
+		);
+	}
+	return tower;
+}
 // 2) ==================================   ---->
-
+function towerBuilder(n) {
+	return Array.from({ length: n }, function (v, k) {
+		const spaces = " ".repeat(n - k - 1);
+		return spaces + "*".repeat(k + k + 1) + spaces;
+	});
+}
 // 3)==================================  ---->
+function towerBuilder(nFloors) {
+	let tower = [];
+	let baseWidth = nFloors * 2 - 1;
 
+	for (let i = 0; i < nFloors; i++) {
+		let blocks = "*".repeat(baseWidth);
+		let blank = " ".repeat(i);
+		let level = blank + blocks + blank;
+
+		tower.unshift(level);
+		baseWidth -= 2;
+	}
+	return tower;
+}
 // 4)==================================  ---->
+function towerBuilder(nFloors) {
+	const repeat = (ch) => (n) => Array(n + 1).join(ch);
+	const space = repeat(" ");
+	const star = repeat("*");
+	const center = (length) => (s) =>
+		space((length - s.length) / 2) + s + space((length - s.length) / 2);
+	const baseSize = nFloors * 2 - 1;
+	const buildFloor = (_, i) => star(2 * i + 1);
 
+	return Array.from({ length: nFloors }, buildFloor).map(center(baseSize));
+}
 // 5)==================================  ---->
+function towerBuilder(nFloors) {
+	var tower = [];
 
+	for (var i = 0; i < nFloors; i++) {
+		var padding = Array(nFloors - i).join(" ");
+
+		tower.push(padding + Array(i * 2 + 2).join("*") + padding);
+	}
+
+	return tower;
+}
 // 6)==================================  ---->
 
 // 7)==================================  ---->
---
+++ 8.If-you-can't-sleep,-just-count-sheep.js
@@ -0,0 +1,36 @@
+//8.If-you-can't-sleep,-just-count-sheep.js
+
+// // 🎹If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+// ⚽ Solution
+
+let countSheep = function (num) {
+	let result = "";
+	for (let i = 1; i <= num; i++) {
+		result += `${i} sheep...`;
+	}
+	return result;
+};
+
+// 1)================================== -->
+
+// 2) ==================================   ---->
+
+// 3)==================================  ---->
+
+// 4)==================================  ---->
+
+// 5)==================================  ---->
+
+// 6)==================================  ---->
+
+// 7)==================================  ---->
+
+//8)==================================  ---->
+
+//9)==================================  ---->
+
+//10)==================================  ---->
