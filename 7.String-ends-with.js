d278b147839e78ac2d2b36838f60303370e30f2c
Liverpool4ik <liverpool4ik@proton.me>
2024-02-25 20:27:50 +0200

    //7.String-ends-with.js

--- 7.String-ends-with.js
+++ 7.String-ends-with.js
@@ -6,24 +6,67 @@
 const solution = (str, ending) => str.endsWith(ending);
 // 1)================================== -->
 
-// function .... {
-
-// }
-
+function solution(str, ending) {
+	if (typeof str != "string" || typeof ending != "string") throw "wrong type";
+	if (ending.length > str.length) return false;
+	return str.substr(str.length - ending.length, ending.length) == ending;
+}
 // 2) ==================================   ---->
-
+function solution(str, ending) {
+	return str.substr(-ending.length) == ending;
+}
 // 3)==================================  ---->
-
+function solution(str, ending) {
+	var l = ending.length;
+	var str = str.slice(-l);
+	return str.match(ending) ? true : false;
+}
 // 4)==================================  ---->
+solution = (s, e) => s.endsWith(e);
 
 // 5)==================================  ---->
+function solution(str, ending) {
+	let strArray = str.split("");
+	let endArray = ending.split("");
+	let wasteArrayLength = strArray.length - endArray.length;
+	let newArray = [];
 
+	for (let i = wasteArrayLength; i < strArray.length; i++) {
+		newArray.push(strArray[i]);
+	}
+
+	let newEnding = endArray.join();
+	let newString = newArray.join();
+
+	if (newString == newEnding) {
+		return true;
+	} else {
+		return false;
+	}
+}
 // 6)==================================  ---->
-
+function solution(str, ending) {
+	return (
+		str
+			.split("")
+			.reverse()
+			.join("")
+			.search(ending.split("").reverse().join("")) === 0
+	);
+}
 // 7)==================================  ---->
-
+function solution(str, ending) {
+	// TODO: complete
+	return str.slice(str.length - ending.length) == ending;
+}
 //8)==================================  ---->
-
+function solution(str, ending) {
+	var test = str.slice(-ending.length);
+	if (test === ending) {
+		return true;
+	}
+	return false;
+}
 //9)==================================  ---->
 
 //10)==================================  ---->
