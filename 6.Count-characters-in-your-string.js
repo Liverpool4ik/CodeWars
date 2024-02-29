365cec53df01deeb22a7184b88eecb87b9005a28
Liverpool4ik <liverpool4ik@proton.me>
2024-02-29 23:23:41 +0200

    6.Count-characters-in-your-string.js

---
+++ 6.Count-characters-in-your-string.js
@@ -0,0 +1,31 @@
+// 6.Count-characters-in-your-string.js
+
+// // 🎹The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+// ⚽ Solution
+function count(string) {
+	//
+	return {};
+}
+// 1)================================== -->
+
+const count = (s) => require("lodash").countBy(s);
+// 2) ==================================   ---->
+
+// 3)==================================  ---->
+
+// 4)==================================  ---->
+
+// 5)==================================  ---->
+
+// 6)==================================  ---->
+
+// 7)==================================  ---->
+
+//8)==================================  ---->
+
+//9)==================================  ---->
+
+//10)==================================  ---->
